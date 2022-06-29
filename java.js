ddd33a00dfef1429e17ea3059cef2bc94553a819
KevinFonteyne <106716748+KevinFonteyne@users.noreply.github.com>
2022-06-29 16:08:50 -0700

    assignment1

---
+++ java.js
@@ -0,0 +1,23 @@
+let songTitle1 = "Equip sunglasses";
+let songTitle2 = "Mystery";
+let songTitle3 = "Takin a walk";
+let songTitle4 = "Damn";
+let songTitle5 = "Sundown"
+let songTitle6 = "Ramen & OJ"
+let songArtist1 ="Hot Mulligan";
+let songArtist2 = "Turnstile";
+let songArtist3 = "Trippie Redd"
+let songArtist4 = "Blink 182"
+let songArtist5 = "Jack Harlow"
+let songArtist6 = "Joyner Lucas"
+let uName = "Kevin"
+let uEmail = "kfonteyne@outlook.com";
+let rating = "5 stars";
+let songLength;
+let album;
+let related;
+let radioStation;
+
+document.write(`
+<p>${songTitle1} by ${songArtist1}</p><p> ${songTitle2} by ${songArtist2} <p>${songTitle3} by ${songArtist3}</p> <p>${songTitle4} by ${songArtist4}</p> <p>${songTitle5} by ${songArtist5}</p> <p> ${songTitle6} by $${songArtist6}
+`)
